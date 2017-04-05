8b255191823b467fd8a619b2a2324b68cedd0a09
niezp <1124578057@qq.com>
2017-04-05 17:19:24 +0800

    ajax search and order

--- efef/Controllers/AccountController.cs
+++ efef/Controllers/AccountController.cs
@@ -13,7 +13,74 @@ namespace efef.Controllers
     public class AccountController : Controller
     {
         private AccountContext db = new AccountContext();
+        public static int Total = 10; 
         public static int TotalPage = 10;
+        public static int pageSize = 5;
+
+
+        //public JsonResult SearchString(string searchString)
+        //{
+        //    db.Configuration.ProxyCreationEnabled = false;
+        //    var useraa = from des in db.SysUsers where (des.Email.Contains(searchString)) select des;
+        //    return Json(useraa, JsonRequestBehavior.AllowGet);
+        //    //var useraa = from des in db.SysUsers where (des.Email.Contains(searchString)) select des ;
+        //    //return Json(useraa, JsonRequestBehavior.AllowGet);
+        //}
+
+        public JsonResult SearchString(string searchString, int? id, string sortOrder)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            ViewBag.PageIndex = id;
+            ViewBag.Total = Total;
+            ViewBag.TotalPage = TotalPage;
+            int pageIndex = 1;
+            if (id.HasValue)
+            {
+                pageIndex = id.Value;
+            }
+            if (string.IsNullOrEmpty(searchString))
+            {
+                ViewBag.NameSortParm = string.IsNullOrWhiteSpace(sortOrder) ? "name_desc" : "";
+                var userdata = ViewBag.NameSortParm == "" ? db.SysUsers.OrderBy(u => u.UserName).Skip((pageIndex - 1) * pageSize).Take(pageSize) : db.SysUsers.OrderByDescending(u => u.UserName).Skip((pageIndex - 1) * pageSize).Take(pageSize);
+
+                return Json(
+                    new { pageIndex = pageIndex, NameSortParm = string.IsNullOrWhiteSpace(sortOrder) ? "name_desc" : "", data = userdata }
+                    , JsonRequestBehavior.AllowGet);
+            }
+            var useraa = from des in db.SysUsers where (des.Email.Contains(searchString)) select des;
+            var user = ViewBag.NameSortParm == "" ? useraa.OrderBy(u => u.UserName).Skip((pageIndex - 1) * pageSize).Take(pageSize) : useraa.OrderByDescending(u => u.UserName).Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            return Json(new { pageIndex= pageIndex, NameSortParm = string.IsNullOrWhiteSpace(sortOrder) ? "name_desc" : "", data = user }, JsonRequestBehavior.AllowGet);
+            //db.Configuration.ProxyCreationEnabled = false;
+            //ViewBag.PageIndex = id;
+            //ViewBag.Total = Total;
+            //ViewBag.TotalPage = TotalPage;
+            //ViewBag.NameSortParm = string.IsNullOrWhiteSpace(sortOrder) ? "name_desc" : "";
+
+            //var useraa = from des in db.SysUsers where (des.Email.Contains(searchString)) select des;
+            //var user = ViewBag.NameSortParm == "" ? useraa.OrderBy(u => u.UserName).Skip((id - 1) * pageSize).Take(pageSize) : useraa.OrderByDescending(u => u.UserName).Skip((id - 1) * pageSize).Take(pageSize);
+            //return Json(user, JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult Sort (string searchString, string sortOrder, int id)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            ViewBag.PageIndex = id;
+            ViewBag.Total = Total;
+            ViewBag.TotalPage = TotalPage;
+            if (string.IsNullOrEmpty(searchString))
+            {
+                ViewBag.NameSortParm = string.IsNullOrWhiteSpace(sortOrder) ? "name_desc" : "";
+                var userdata = ViewBag.NameSortParm == "" ? db.SysUsers.OrderBy(u => u.UserName).Skip((id - 1) * pageSize).Take(pageSize) : db.SysUsers.OrderByDescending(u => u.UserName).Skip((id - 1) * pageSize).Take(pageSize);
+
+                return Json(
+                    new { NameSortParm = string.IsNullOrWhiteSpace(sortOrder) ? "name_desc" : "", data = userdata }
+                    , JsonRequestBehavior.AllowGet);
+            }
+            var useraa = from des in db.SysUsers where (des.Email.Contains(searchString)) select des;
+            var user = ViewBag.NameSortParm == "" ? useraa.OrderBy(u => u.UserName).Skip((id - 1) * pageSize).Take(pageSize) : useraa.OrderByDescending(u => u.UserName).Skip((id - 1) * pageSize).Take(pageSize);
+            return Json(new { NameSortParm= string.IsNullOrWhiteSpace(sortOrder) ? "name_desc" : "",data = user }, JsonRequestBehavior.AllowGet);
+
+        }
         // GET: Account
         /// <summary>
         /// 获取列表
@@ -22,11 +89,11 @@ namespace efef.Controllers
         public ActionResult Index()
         {
             ViewBag.PageIndex = 1;
-            ViewBag.Total = 20;
+            ViewBag.Total = Total;
             ViewBag.TotalPage = TotalPage;
-            int pageSize = 5;
+ 
             int i = 1;
-            var user = db.SysUsers.OrderBy(u => u.UserName).Skip((i - 1) * pageSize).Take(5);
+            var user = db.SysUsers.OrderBy(u => u.UserName).Skip((i - 1) * pageSize).Take(pageSize);
             return View(user);
         }
 
@@ -42,10 +109,10 @@ namespace efef.Controllers
                 i = 1;
             }
             ViewBag.PageIndex = i;
-            ViewBag.Total = 20;
+            ViewBag.Total = Total;
             ViewBag.TotalPage = TotalPage;
-            int pageSize = 5;
-            var user = db.SysUsers.OrderBy(u => u.UserName).Skip((i - 1) * pageSize).Take(5);
+  
+            var user = db.SysUsers.OrderBy(u => u.UserName).Skip((i - 1) * pageSize).Take(pageSize);
             return View("Index", user);
         }
 
